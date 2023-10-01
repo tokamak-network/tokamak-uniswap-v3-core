39fa7b52a6b7e2e0d30167c2ea59a0a9da71bffc
지의신 <64007655+usgeeus@users.noreply.github.com>
2023-10-01 12:52:27 +0900

    add UniswapV3PoolSwapTest Verify

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -7,7 +7,20 @@ let data = JSON.parse(fs.readFileSync(`deployed.uniswap.${chainName}.json`).toSt
 
 const main = async () => {
   console.log('Verifying contracts...')
-  const contractAddress = data['UniswapV3Factory']
+  let contractAddress = data['UniswapV3Factory']
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: [],
+    })
+  } catch (e) {
+    if (e.meesage.toLowerCase().includes('already verified')) {
+      console.log('Contract already verified')
+    } else {
+      console.log(e)
+    }
+  }
+  contractAddress = data['UniswapV3PoolSwapTest.sol']
   try {
     await run('verify:verify', {
       address: contractAddress,
